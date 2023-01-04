3f5cdf7a5c42faa3f5c82f68feafdb5ea2410b2b
WilliamFun <1509325537@qq.com>
2023-01-05 01:17:12 +0800

    finish

--- src/test/java/com/citics/logdemo/service/impl/LogFileServiceImplTest.java
+++ src/test/java/com/citics/logdemo/service/impl/LogFileServiceImplTest.java
@@ -11,6 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = LogdemoApplication.class)
@@ -29,18 +36,43 @@ class LogFileServiceImplTest {
 
     @Test
     public void logMatchByWord() {
+        logFileService.setServer("serverA");
+        String service = "bussiness-interface";
+        Date date = new Date(2022-1900, Calendar.DECEMBER,30);
+        List<String> toFinds = new ArrayList<>();
+        toFinds.add("生成");
+        toFinds.add("耗时");
+        logFileService.logMatchByWord(service,date,toFinds);
     }
 
     @Test
     public void logMatchByTime() {
+        logFileService.setServer("serverA");
+        String service = "bussiness-interface";
+        Date date = new Date(2022-1900, Calendar.DECEMBER,30);
+        SimpleDateFormat formatter=new SimpleDateFormat("HH:mm:ss");
+        try {
+            Date start = formatter.parse("07:00:00");
+            Date end = formatter.parse("08:00:00");
+            logFileService.logMatchByTime(service,date,start,end);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
     public void splitLogFileByNum() {
+        logFileService.setServer("serverA");
+        String service = "bussiness-interface";
+        Date date = new Date(2021-1900, Calendar.AUGUST,30);
+        logFileService.splitLogFileByNum(service,date,3);
     }
 
     @Test
     public void splitLogFileBySize() {
-
+        logFileService.setServer("serverA");
+        String service = "bussiness-interface";
+        Date date = new Date(2021-1900, Calendar.AUGUST,30);
+        logFileService.splitLogFileBySize(service,date,"500K");
     }
 }
